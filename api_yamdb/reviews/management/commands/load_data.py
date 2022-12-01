ae26d6c9406f6c1ce7bfb515fdfed67916d402c9
«Natalia <natalipogorelova1971@gmail.com>
2022-12-01 14:08:55 +0300

    Обновление с Readme

--- api_yamdb/reviews/management/commands/load_data.py
+++ api_yamdb/reviews/management/commands/load_data.py
@@ -13,12 +13,18 @@ TABLES = {
     Review: 'review.csv',
     Comment: 'comments.csv',
 }
+MANYTOMANY = {'genre_title.csv': {
+    'title_id': Title,
+    'genre_id': Genre,
+    'order': ['title_id', 'genre_id']
+}}
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for model, csv_f in TABLES.items():
+            model.objects.all().delete()
             with open(
                 f'{settings.BASE_DIR}/static/data/{csv_f}',
                 'r',
@@ -27,4 +33,16 @@ class Command(BaseCommand):
                 reader = csv.DictReader(csv_file)
                 model.objects.bulk_create(
                     model(**data) for data in reader)
+        for csv_f, links in MANYTOMANY:
+            with open(
+                f'{settings.BASE_DIR}/static/data/{csv_f}',
+                'r',
+                encoding='utf-8'
+            ) as csv_file:
+                reader = csv.DictReader(csv_file)
+                for row in reader:
+                    target = links[links[order[0]]].objects.get(id=row[links[order[0]]])
+                    sub = row[links[order[1]]]
+                    target.genre.add(sub)        
         self.stdout.write(self.style.SUCCESS('Все данные загружены'))
+        
